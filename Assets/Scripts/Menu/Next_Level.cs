e828ea0eabc5e9fc43d0a6fb20cb5c46cfb39c40
Foxy-TX <159435858+Foxy-TX@users.noreply.github.com>
2024-05-09 08:37:02 -0300

    Troca de cena

---
+++ Assets/Scripts/Menu/Next_Level.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next_Level : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
