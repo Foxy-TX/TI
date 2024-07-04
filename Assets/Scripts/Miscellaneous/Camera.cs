80855bd7b0d41acaa7783de92426bfcbea0c8b56
Foxy-TX <159435858+Foxy-TX@users.noreply.github.com>
2024-07-04 09:46:32 -0300

    Agora Acabou

--- Assets/Scripts/Miscellaneous/Camera.cs
+++ Assets/Scripts/Miscellaneous/Camera.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class Camera : MonoBehaviour
     public Transform alvo;
 
     public float speed = 5;
+
+    public float distancia = 1.0f;
+
+    public bool lvl1;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +29,18 @@ public class Camera : MonoBehaviour
 
         transform.Translate(direction * speed * Time.deltaTime);
 
-        
+        if (Vector3.Distance(alvo.position, transform.position) > distancia)
+        {
+            speed = 20;
+        }
+        else
+        {
+            if (lvl1 == true) {
+                speed = 3;
+            }
+            else {
+                speed = 5; 
+            }
+        }
     }
 }
